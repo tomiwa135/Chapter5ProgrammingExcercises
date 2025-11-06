cdb9fdda161fdcf805d63f3164e6c76bb0bc2e83
Tomiwa Akinrotim <tomiwa.akinrotimi@gmail.com>
2025-11-06 08:29:42 -0600

    Re-added example files

---
+++ Number2_rearrangeValueOfPI.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	double pi = 0;
+	long i;
+	long n;
+
+	cout << "Enter the value of n: ";
+	cin >> n;
+	cout << endl;
+
+	pi = 0;
+
+	for (i = 0; i < n; i++) {
+
+		if (i % 2 == 0) 
+		{
+			pi = pi + (1 / (2 * i + 1));
+		}
+		else 
+		{
+			pi = pi - (1 / (2 * i + 1));
+		}
+	}
+
+	pi = 4 * pi;
+	cout << endl << "pi = " << pi << endl;
+
+	return 0;
+
+}
